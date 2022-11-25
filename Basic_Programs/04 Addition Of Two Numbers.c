367ba3686855a01d6922fb0b9f2130f192b3710e
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-11-25 18:57:10 +0530

    Add files via upload

---
+++ Basic_Programs/04 Addition Of Two Numbers.c
@@ -0,0 +1,15 @@
+//Compiler version gcc  6.3.0
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int No1 = 60, No2 = 35, Sum = 0;
+       
+       Sum = No1 + No2;
+       
+       printf("\n Addition Of %d + %d = %d",No1,No2,Sum);
+       
+       getch();
+       return 0;
+}
