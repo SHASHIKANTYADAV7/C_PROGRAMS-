e04a308d20a33484b78947b2ad863908f0cfa4bb
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-11-26 13:33:44 +0530

    Add files via upload

---
+++ Basic_Programs/07 while_Statement.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+      int i = 1;
+
+      while(i <= 10)
+      {
+           printf("\n %d",i);
+           printf("\n\t I LOVE INDIA...");
+           i++;
+      }
+
+      getch();
+      return 0;
+}
