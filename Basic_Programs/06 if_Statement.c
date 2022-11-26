7b0f30b4da8c141cd1af2d84cf8de76d7c62012a
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-11-26 13:33:15 +0530

    Add files via upload

---
+++ Basic_Programs/06 if_Statement.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       float Amount = 0.0;
+
+       ptintf("\n Enter Your Account Balance => ");
+       scanf("%f",&Amount);
+
+       if(Amount > 1000)
+       {
+            printf("\n\t WELCOME TO MALL...!");
+       }
+            printf("\n Thanks For Visit...");
+
+       getch();
+       return 0;
+}
