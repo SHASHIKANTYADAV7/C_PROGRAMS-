a56da6053a1c0c04c0db82663ea327ec112d8612
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-11-27 08:45:11 +0530

    Add files via upload

---
+++ Basic_Programs/08 for_Statement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int i = 10;
+
+     for(i <= 10);
+     {
+          printf("\n %d,i");
+          printf("\n\t WELCOME");
+          printf("\n I LOVE INDIA....");
+          i++;
+     }
+
+     getch();
+     return 0;
+}
