fa688cf6d79f9523f83e88f66db5a47fc6ffa476
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-05 08:47:55 +0530

    Add files via upload

---
+++ Conditional_Control_Statements_Programs/18 Positive_OR_Negative_Number_Approach_04.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int Num = 0;
+
+       Up:
+       printf("\n Enter Your Number => ");
+       scanf("%d",&Num);
+
+       if(Num == 0)
+       {
+            printf("\n\t %d Is Neutral Number.\n\t Please Enter Another Number");
+            goto Up;
+       }
+       if(Num > 0)
+       {
+            printf("\n %d Is Positive Number",Num);
+       }
+       else
+       {
+            printf("\n %d Is Negative Number",Num);
+       }
+
+            printf("\n Thanks....");
+
+       getch();
+       return 0;
+}
