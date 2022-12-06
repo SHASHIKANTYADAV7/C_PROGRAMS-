724059153a6ec8687737a0fe125fb34210ed9ede
SHASHIKANTYADAV7 <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-06 12:22:37 +0530

    Add files via upload

---
+++ Conditional_Control_Statements_Programs/22 Cube_Of_Numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int Num = 0;
+
+       Up:
+       printf("\n Enter Positive Integer Number => ");
+       scanf("%d",&Num);
+
+       if(Num <= 0)
+       {
+             printf("\n\t Invalid Number.\n\t Please Enter Another Number");
+             getch();
+             system("cls");
+             goto Up;
+       }
+
+       printf("\n\t Cube Of %d = %d",Num,Num * Num * Num);
+
+       printf("\n Thanks...");
+
+       getch();
+       return 0;
+}
