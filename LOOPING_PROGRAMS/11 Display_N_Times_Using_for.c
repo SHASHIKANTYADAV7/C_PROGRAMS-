acfe7cbb2015e1d90882a6e97b95d5f809eee6b4
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-14 12:11:09 +0530

    Add files via upload

---
+++ LOOPING_PROGRAMS/11 Display_N_Times_Using_for.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int Num = 0, Cnt = 0;
+
+       printf("\n Enter positive Integer Number For Printing Name Of Class => ");
+       scanf("%d",&Num);
+
+       system("cls");
+       printf("\n\n\t=============*****============\n\n");
+
+       for(Cnt = 1; Cnt<= Num; Cnt++)
+       {
+            printf("\n\t SHASHI YADAV");
+       }
+
+       getch();
+       return 0;
+}
+
+
