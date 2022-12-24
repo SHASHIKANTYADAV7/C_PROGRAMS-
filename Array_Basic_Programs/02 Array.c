7cfdb3349a967fb54a8634e119898a910e9f4472
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-24 12:40:57 +0530

    Add files via upload

---
+++ Array_Basic_Programs/02 Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int Num[] = {34, 55, 44, 56, 32, 58};
+
+       printf("\n\n The Value Of 1st Number = %d",Num[0]);
+       printf("\n\n The Value Of 2nd Number = %d",Num[1]);
+       printf("\n\n The Value Of 3rd Number = %d",Num[2]);
+       printf("\n\n The value Of 4th Number = %d",Num[3]);
+       printf("\n\n The Value Of 5th Number = %d",Num[4]);
+       printf("\n\n The Value Of 6th Number = %d",Num[5]);
+
+       printf("\n\n\t Thanks...");
+
+       getch();
+       return 0;
+}
