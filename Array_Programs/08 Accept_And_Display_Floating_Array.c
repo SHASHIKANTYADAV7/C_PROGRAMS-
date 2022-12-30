e7c76307da32aa8ed75bb310d68111b6f63c9ac5
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-30 14:47:49 +0530

    Add files via upload

---
+++ Array_Programs/08 Accept_And_Display_Floating_Array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        float Per[5] = {'\0'};
+
+        printf("\n Enter Percentage Of 1st Student => ");
+        scanf("%f",&Per[0]);
+
+        printf("\n Enter Percentage Of 2nd Student => ");
+        scanf("%f",&Per[1]);
+
+        printf("\n Enter Percentage Of 3rd Student => ");
+        scanf("%f",&Per[2]);
+
+        printf("\n Enter Percentage Of 4th Student => ");
+        scanf("%f",&Per[3]);
+
+        printf("\n Enter Percentage Of 5th Student => ");
+        scanf("%f",&Per[4]);
+
+        getch();
+        return 0;
+}
