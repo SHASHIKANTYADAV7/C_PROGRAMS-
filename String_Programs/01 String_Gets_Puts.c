ec82a04359d526dad4f5bc38e870a2d5087d8f5c
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2022-12-31 10:13:13 +0530

    Add files via upload

---
+++ String_Programs/01 String_Gets_Puts.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       char cArr[20] = {'K', 'A', 'B', 'C', 'D'};
+
+       puts(cArr);
+
+       puts("\n Enter A String: ");
+       gets(cArr);
+
+       puts("\n Given String Is => ");
+       gets(cArr);
+
+       getch();
+       return 0;
+}
