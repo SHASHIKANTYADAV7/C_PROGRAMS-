f329a58799731bcad1d752c2fc932d5232d130ef
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2023-01-01 08:29:24 +0530

    Add files via upload

---
+++ String_Programs/02 String_Length_In_Main_while.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       char cSrc[50] = {'\0'};
+       int Len = 0;
+
+       puts("\n Enter A String => ");
+       gets(cSrc);
+
+       while( cSrc[Len] != 0)
+       {
+           Len++;
+       }
+
+       printf("\n Length Of Given String Is = %d",Len);
+
+       getch();
+       return 0;
+}
