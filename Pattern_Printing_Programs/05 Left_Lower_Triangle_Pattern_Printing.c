13daeb116a0d47bda5503ad16c700fe85d5fc40f
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2023-01-08 12:07:07 +0530

    Add files via upload

---
+++ Pattern_Printing_Programs/05 Left_Lower_Triangle_Pattern_Printing.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int r = 0, c = 0, Cnt = 0;
+
+       printf("\n Enter Count Rows And Colums => ");
+       scanf("%d",&Cnt);
+
+       printf("\n Pattern Is => \n\n");
+
+       for(r = 1; r <= Cnt; r++)
+       {
+           for(c = 1; c <= Cnt; c++)
+           {
+               if(r >= c)
+               {
+                   printf(" * ");
+               }
+           }
+           printf("\n");
+       }
+
+       printf("\n Thanks...");
+
+       getch();
+       return 0;
+}
