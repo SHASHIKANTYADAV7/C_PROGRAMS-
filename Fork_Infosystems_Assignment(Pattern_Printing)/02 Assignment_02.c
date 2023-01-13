8783cafb6575203b98ce3aa54ecc0dc331b92f09
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2023-01-13 10:19:44 +0530

    Add files via upload

---
+++ Fork_Infosystems_Assignment(Pattern_Printing)/02 Assignment_02.c
@@ -0,0 +1,35 @@
+//A
+//F G
+//K L M
+//P Q R S
+//U V W X Y
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+        int r = 0, c = 0, Cnt = 0;
+        char ch = 'A';
+
+        printf("\n Enter Count Of Rows And Column => ");
+        sacnf("%d,&Cnt");
+
+        printf("\n Pattern Is => \n");
+
+        for(r = 1; r <= Cnt; r++)
+        {
+            for(c =1; c <= Cnt; c++)
+            {
+                if(r >= c)
+                {
+                    printf("%c",ch);
+                }
+                ch++;
+            }
+            printf("\n");
+        }
+
+        getch();
+        return 0;
+}
+
