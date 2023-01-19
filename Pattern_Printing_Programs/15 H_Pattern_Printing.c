75da47f7bbb8de03e23b9a5498e8f0a1eabfd136
SHASHIKANT_YADAV <118982586+SHASHIKANTYADAV7@users.noreply.github.com>
2023-01-19 21:34:29 +0530

    Add files via upload

---
+++ Pattern_Printing_Programs/15 H_Pattern_Printing.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+       int r = 0, c = 0, Cnt = 0;
+
+       printf("\n Enter Count Of Rows And Columns => ");
+       scanf("%d",&Cnt);
+
+       printf("\n Pattern Is => \n");
+
+       for(r = 1; r <= Cnt; r++)
+       {
+           for(c = 1; c <= Cnt; c++)
+           {
+
+               if(c == 1 || c == Cnt || r == Cnt/2+1)
+               {
+                   printf(" * ");
+               }
+               else
+               {
+                   printf("   ");
+               }
+         }
+         printf("\n");
+      }
+
+       getch();
+       return 0;
+}
